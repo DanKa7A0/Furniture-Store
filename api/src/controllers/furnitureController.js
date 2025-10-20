b26796db7542ba641a007645d7b04d5ac752b88d
dibr <daniel.bratov@unicontsoft.com>
2025-10-20 16:22:42 +0300

    Add furniture service

--- api/src/controllers/furnitureController.js
+++ api/src/controllers/furnitureController.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import { furnitureService } from "../services/index.js";
 
 const furnitureController = Router();
 
-furnitureController.get("/", (req, res) => {    
+furnitureController.get("/", (req, res) => {
     res.status(200).json([]);
+});
+
+furnitureController.post("/", (req, res) => {
+    furnitureService.CreateFurniture(req.body);
+    res.end();
 });
 
 export default furnitureController;
---
+++ api/src/services/furnitureService.js
@@ -0,0 +1,3 @@
+export async function CreateFurniture(userData){
+    console.log(userData);
+}
